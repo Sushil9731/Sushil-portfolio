c8897340801e2df58ddbb78702a3d44520bd7aba
Sushil Chaudhari <chaudharisushil96@gmail.com>
2025-12-25 15:38:30 +0530

    Updated folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
-// import Projects from '@/components/Projects'
+import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
 import Footer from '@/components/Footer'
 
